c63487596ef4ea0af51ae926e090ac1ece166c69
amankumar <asr250901@gmail.com>
2026-01-29 12:58:37 +0530

    Added Transaction.java class

---
+++ src/main/java/net/banking/banking_app/entity/Transaction.java
@@ -0,0 +1,28 @@
+package net.banking.banking_app.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "transaction")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private long id;
+    private long accountId;
+    private  double amount;
+    private  String transactionType;
+    private LocalDateTime timestamp;
+
+}
